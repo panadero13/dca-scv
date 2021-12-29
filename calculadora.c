afee68b19bf1337155e5cf434ce638c0d1e45078
Luis Miguel Panadero Cerdá <lmpc3@alu.ua.es>
2021-12-29 17:12:24 +0100

    Añadido codigo funete calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    char opcion; 
+    double n1, n2;
+
+    do
+    {
+        printf( "\n   >>> MENU CALCULADORA <<<" );
+        printf( "\n\n   1. Sumar dos numeros.");
+        printf( "\n   2. Restar dos numeros.");
+        printf( "\n   3. Multiplicar dos numeros.");
+        printf( "\n   4. Dividir dos numeros.");
+        printf( "\n   5. Cuadrado de un numero.");
+        printf( "\n   6. Raiz cuadrada de un numero.");
+        printf( "\n   7. Salir.\n" );
+
+        /* Filtramos la opción elegida por el usuario */
+        do
+        {
+            printf( "\n   Introduzca opcion (1-7): ");
+            fflush( stdin );
+            scanf( "%c", &opcion);
+
+        } while ( opcion < '1' || opcion > '7' );
+        /* La opción sólo puede ser '1', '2', '3', '4' , '5', '6' o '7' */
+
+        switch ( opcion )
+        {
+                      /* Opción 1: Sumar */
+            case '1': printf( "\n   Introduzca primer sumando: " );
+                      scanf( "%lf", &n1);
+                      printf( "\n   Introduzca segundo sumando: " );
+                      scanf( "%lf", &n2);
+                      printf( "\n   %lf + %lf = %lf\n", n1, n2, n1 + n2 );
+                      break;
+
+                      /* Opción 2: Restar */
+            case '2': printf( "\n   Introduzca minuendo: " );
+                      scanf( "%lf", &n1);
+                      printf( "\n   Introduzca sustraendo: " );
+                      scanf( "%lf", &n2);
+                      printf( "\n   %lf - %lf = %lf\n", n1, n2, n1 - n2 );
+                      break;
+
+                      /* Opción 3: Multiplicar */
+            case '3': printf( "\n   Introduzca primer operando: " );
+                      scanf( "%lf", &n1);
+                      printf( "\n   Introduzca segundo operando: " );
+                      scanf( "%lf", &n2);
+                      printf( "\n   %lf * %lf = %lf\n", n1, n2, n1 * n2 );
+                      break;
+
+                      /* Opción 4: División entera */
+            case '4': printf( "\n   Introduzca dividendo: " );
+                      scanf( "%lf", &n1);
+                      printf( "\n   Introduzca divisor: " );
+                      scanf( "%lf", &n2);
+                      if ( n2 != 0 )
+                          printf( "\n   %lf div %lf = %lf\n", n1, n2, n1 / n2 );
+                      else
+                          printf( "\n   ERROR: No se puede dividir entre cero.\n" );
+                      break;
+
+                      /* Opción 5: Cuadrado de un numero */
+            case '5': printf( "\n   Introduzca numero a elevar al cuadrado: " );
+                      scanf( "%lf", &n1);
+                      printf( "\n   %lf ^ 2 = %lf\n", n1, n1 * n1 );
+                      break;
+
+                      /* Opción 6: Raiz cuadrada de un numero */
+            case '6': printf( "\n   Introduzca numero que radicar: " );
+                      scanf( "%lf", &n1);
+                      printf( "\n   Raiz de %lf = %lf\n", n1, sqrt(n1) );
+                      break;
+        }
+    } while ( opcion != '7' );
+
+    return 0;
+}
